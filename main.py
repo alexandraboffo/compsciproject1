7805a2d1b9706b691af21aea7b95490008cb1c59
Alexandra Boffo <alexandraboffo@MacBook-Air-6.local>
2022-09-28 18:44:40 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Name: Alexandra Boffo  Date: September 28, 2022
+# Boffo-Invoice.py
+y = "*"*50
+print(y)
+print("\t\tColumbus State Community College\n\t\t550 East Spring Street\n\t\tColumbus, OH 43215")
+x = "-"*50
+print(x)
+print("\t\tProduct Name: \t Product Price")
+b = "52.99"
+l = "25.00"
+t = 157.93*3
+print("\t\tBooks             $",b)
+print("\t\tLab Fee           $",l)
+print("\t\tTuition           $",t)
+print(x)
+print("                          Total")
+sum = float(b) + float(l) + float(t)
+print("                          $",str(sum))
+print(x)
+print("   Thank you for being a Columbus State Student")
+print(y)
+
+
+
+
